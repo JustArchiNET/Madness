538fabfbdbdcfedb9e6ffb79e95a017d74b31f34
Archi <JustArchi@JustArchi.net>
2021-11-11 23:11:55 +0100

    Implement Environment static class

---
+++ JustArchiNET.Madness/EnvironmentMadness/Environment.cs
@@ -0,0 +1,51 @@
+//                             _         __  __
+//  ___  ___   ___  _ __    __| |  __ _ |  \/  |
+// / __|/ __| / _ \| '_ \  / _` | / _` || |\/| |
+// \__ \\__ \|  __/| | | || (_| || (_| || |  | |
+// |___/|___/ \___||_| |_| \__,_| \__,_||_|  |_|
+// |
+// Copyright 2021-2021 Łukasz "JustArchi" Domeradzki
+// Contact: JustArchi@JustArchi.net
+// |
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// |
+// http://www.apache.org/licenses/LICENSE-2.0
+// |
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+using JetBrains.Annotations;
+using JustArchiNET.Madness.Helpers;
+
+namespace JustArchiNET.Madness.EnvironmentMadness;
+
+[MadnessType(EMadnessType.Replacement)]
+[PublicAPI]
+public static class Environment {
+	[MadnessType(EMadnessType.Proxy)]
+	public static string NewLine => System.Environment.NewLine;
+
+	[MadnessType(EMadnessType.Implementation)]
+	public static string? ProcessPath {
+		get {
+			using Process process = Process.GetCurrentProcess();
+
+			return process.MainModule?.FileName;
+		}
+	}
+
+	[MadnessType(EMadnessType.Proxy)]
+	public static void Exit(int exitCode) => System.Environment.Exit(exitCode);
+
+	[MadnessType(EMadnessType.Proxy)]
+	public static string[] GetCommandLineArgs() => System.Environment.GetCommandLineArgs();
+
+	[MadnessType(EMadnessType.Proxy)]
+	public static string? GetEnvironmentVariable(string variable) => System.Environment.GetEnvironmentVariable(variable);
+}
